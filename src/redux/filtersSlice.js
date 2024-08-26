68bc9d90bc2e5569157b166a54c8788d31b5936c
Ruslan-Chyzhenko <144059463+Ruslan-Chyzhenko@users.noreply.github.com>
2024-08-26 20:31:10 +0200

    start

---
+++ src/redux/filtersSlice.js
@@ -0,0 +1,8 @@
+// reducers: {
+//  deleteContact: (state, action) => {
+//   state.contacts = state.contacts.filter(contact => contact.id !== action.payload)
+//  },
+//  addContact: (state, action) => {
+//   state.contacts = [...state.contacts, action.payload]
+//  },
+// },
