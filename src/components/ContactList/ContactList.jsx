3f1cd49b073d3488d25e4570a1566f757d46ae33
Ruslan-Chyzhenko <144059463+Ruslan-Chyzhenko@users.noreply.github.com>
2024-08-27 22:11:40 +0200

    finish_001

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -20,7 +20,7 @@ export default function ContactList() {
   return (
     <div className={css.gridContainer}>
       {visibleContacts.map((contact) => (
-        <li className={css.item} key={id}>
+        <li className={css.item} key={contact.id}>
           <Contact data={contact} onDelete={handleDeleteContact} />
         </li>
       ))}
