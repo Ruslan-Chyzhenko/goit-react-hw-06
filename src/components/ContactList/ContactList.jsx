60b26fe615d1712810ec0291c43300ca5a2b3189
Ruslan-Chyzhenko <144059463+Ruslan-Chyzhenko@users.noreply.github.com>
2024-08-27 15:35:33 +0200

    process_006

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,10 +1,10 @@
 import Contact from "../Contact/Contact";
 import css from "./ContactList.module.css";
 
-export default function ContactList({ tasks, onDelete }) {
+export default function ContactList({ contacts, onDelete }) {
   return (
     <div className={css.gridContainer}>
-      {tasks.map((task) => (
+      {contacts.map((contact) => (
         <li className={css.item} key={task.id}>
           <Contact data={task} onDelete={onDelete} />
         </li>
